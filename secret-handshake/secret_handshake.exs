e57b20abae9a318faf65aa3f50e1892f48d0c4e4
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-08-11 15:55:08 +0700

    added secret-handshake

---
+++ secret-handshake/secret_handshake.exs
@@ -0,0 +1,39 @@
+defmodule SecretHandshake do
+
+  @reverser 16
+
+  def commands(code) when rem(code, @reverser) == 0, do: []
+
+  def commands(code) do
+    handshakes = do_commands([], code)
+    if code / @reverser > 1 do
+      handshakes |> Enum.reverse
+    else
+      handshakes
+    end
+  end
+
+  def do_commands(code) when code < 1, do: []
+  def do_commands(handshakes, 0), do: handshakes
+
+  def do_commands(handshakes, code) when code - 16 >= 0  do
+    do_commands(handshakes, code - 16)
+  end
+
+  def do_commands(handshakes, code) when code - 8 >= 0  do
+    do_commands(["jump" | handshakes], code - 8)
+  end
+
+  def do_commands(handshakes, code) when code - 4 >= 0 do
+    do_commands(["close your eyes" | handshakes], code - 4)
+  end
+
+  def do_commands(handshakes, code) when code - 2 >= 0 do
+    do_commands(["double blink" | handshakes], code - 2)
+  end
+
+  def do_commands(handshakes, code) when code - 1 >= 0 do
+    do_commands(["wink" | handshakes], code - 1)
+  end
+
+end
