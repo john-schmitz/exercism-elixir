816fd3c88dde40faf434be5ba08ef173c048b9eb
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-08-13 15:33:35 +0700

    added space-age

---
+++ space-age/space_age.exs
@@ -0,0 +1,26 @@
+defmodule SpaceAge do
+  @orbital_difference %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :uranus | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    (seconds / earth_seconds_per_year) / @orbital_difference[planet]
+  end
+
+  defp earth_seconds_per_year, do: 3600 * 24 * 365.25
+end
