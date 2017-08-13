4709f20cd0eb9ba167cb37953a730e766c221dc5
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-08-13 18:16:47 +0700

    added word-count

---
+++ word-count/word_count.exs
@@ -0,0 +1,19 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+    sentence
+    |> String.replace(~r<,|!|&|@|\$|%|\^|:>, "")
+    |> String.split(~r< |_>, trim: true)
+    |> do_count(%{})
+  end
+
+  def do_count([], result), do: result
+  def do_count([head | tail], result) do
+    do_count(tail, Map.update(result, String.downcase(head), 1, fn(current_value) -> current_value + 1 end))
+  end
+end
