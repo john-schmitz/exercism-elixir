fb44c975bb0e8cb645a869ffba6accd279cdecd2
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-08-18 16:54:27 +0700

    added sum-of-multiples

---
+++ sum-of-multiples/sum_of_multiples.exs
@@ -0,0 +1,11 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors) do
+    (for x <- 1..(limit-1), y <- factors, rem(x, y) == 0, do: x)
+    |> Enum.uniq
+    |> Enum.sum
+  end
+end
