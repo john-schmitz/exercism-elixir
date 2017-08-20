b6abcaf56a53d87a3fc382c86e42614e1e8f3eea
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-08-20 12:19:59 +0700

    added rna-transcription

---
+++ rna-transcription/rna_transcription.exs
@@ -0,0 +1,25 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna
+    |> Enum.map(&(Map.get(dna_to_rna_mapping(), [&1])))
+    |> List.flatten
+  end
+
+  def dna_to_rna_mapping do
+    %{
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+  end
+end
