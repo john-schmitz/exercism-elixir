efa05cc8c82dc15c8f4f1ed07ac098c202daafd4
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-09-03 11:49:09 +0700

    meetup.exs, adding doc and renaming some variables

--- meetup/meetup.exs
+++ meetup/meetup.exs
@@ -17,25 +17,39 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: :calendar.date
   def meetup(year, month, weekday, schedule) do
+    # Try to find out, what wday(1 to 7 related to monday to sunday)
+    # is the very first wday of the valid date range.
+    # :teenth starts with 13.
     starter_day = case schedule do
                     :teenth -> 13
                     _ -> 1
                   end
 
     {:ok, first_date} = Date.new(year, month, starter_day)
-    first_wday = Date.day_of_week(first_date)
+    starter_wday = Date.day_of_week(first_date)
 
+    # Scheduled wday is the number of wday related to scheduled weekday.
     scheduled_wday = weekday |> day_to_wday
-    diff = 8 - (first_wday - scheduled_wday)
-    scheduled_day = case diff > 7 do
+
+    # Finding the day difference between the starting wday and the scheduled one.
+    # 1 week has 7 days, it is basically like base 7 calculation.
+    # We find the wday difference between the starter and the scheduled one.
+    # Let 8 be subtracted by that number, if the result is more than 7, we
+    # rotate it by subtracting it by 7.
+    diff = 8 - (starter_wday - scheduled_wday)
+    scheduled_wday = case diff > 7 do
                       true -> diff - 7
                       false -> diff
                     end
 
-    day = scheduled_day + Map.get(days_adder(), schedule)
+    # Second, third, fourth weeks just add the number by
+    # multiplication of 7. Teenth starts with 12. Last naively starts with 21.
+    day = scheduled_wday + Map.get(days_adder(), schedule)
+
+    # Just in case the day is not the last wday of the month, just add it by 7.
     calculated_day = cond do
       schedule == :last && Date.days_in_month(first_date) >= day + 7
-      -> day + 7
+        -> day + 7
       true -> day
     end
 
