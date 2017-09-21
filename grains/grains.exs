07b77d987a1ff996ad56c7ee01817ed3792e1892
dwi wahyudi <dwi.wahyudi@happyfresh.com>
2017-09-21 20:18:43 +0700

    grains added

---
+++ grains/grains.exs
@@ -0,0 +1,22 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer) :: pos_integer
+  def square(number) when number not in 1..64, do:
+    { :error, "The requested square must be between 1 and 64 (inclusive)" }
+  def square(number, result \\ 1)
+  def square(1, result), do: {:ok, result}
+  def square(number, result), do: square(number - 1, result * 2)
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: pos_integer
+  def total do
+    result = Enum.reduce(1..64, 0, fn(x, acc) ->
+      with {:ok, squared} <- square(x), do: acc + squared
+    end)
+    {:ok, result}
+  end
+end
